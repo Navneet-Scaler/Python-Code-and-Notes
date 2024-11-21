7e91800ba5d12b45cfd5f8fb5135d13751c38e83
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2024-11-21 12:17:52 +0530

    Files added

---
+++ Basic/3Syntax_Indenation_Comment.py
@@ -0,0 +1,17 @@
+"""
+Python Syntax
+1.Python has simple and clean syntax, making it highly readable. Key rules include:
+2.Case-sensitive: name and Name are different variables.
+3.Statements and Line Breaks: Each statement is typically written on a new line.
+End of Statement: No semicolon (;) is needed, although it can be used optionally.
+
+Indentation
+1.Python uses indentation to define blocks of code instead of braces {} or keywords.
+2.Each level of indentation must be consistent (spaces or tabs).
+
+Comments
+Comments are ignored by the Python interpreter and are used for documentation.
+"""
+
+# Printing Output
+print("Hello World!")
